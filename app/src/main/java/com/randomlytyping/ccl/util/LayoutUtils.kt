9e851f22a984d0b64f8b0f69abbdc9c8a52de951
Huyen Tue Dao <huyen@randomlytyping.com>
2017-04-10 20:40:25 -0400

    Pull app bar logic into an extension function.

--- app/src/main/java/com/randomlytyping/ccl/util/LayoutUtils.kt
+++ app/src/main/java/com/randomlytyping/ccl/util/LayoutUtils.kt
@@ -5,9 +5,11 @@ package com.randomlytyping.ccl.util
 import android.support.annotation.IdRes
 import android.support.annotation.LayoutRes
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import butterknife.ButterKnife
+import com.randomlytyping.ccl.R
 
 // Utility methods / extensions related to activity layouts
 
@@ -19,3 +21,15 @@ fun <T : ViewGroup> AppCompatActivity.inflateInto(@IdRes containerId: Int,
     ButterKnife.findById<T>(this, containerId)?.apply {
       LayoutInflater.from(this@inflateInto).inflate(layoutResId, this)
     } ?: throw IllegalArgumentException("Activity does not have a container with id $containerId")
+
+/**
+ * Sets up an activity's app bar.
+ */
+fun AppCompatActivity.setUpAppBar() {
+  // Set up app bar.
+  setSupportActionBar(ButterKnife.findById<Toolbar>(this, R.id.app_bar))
+  supportActionBar?.also {
+    it.setDisplayShowHomeEnabled(true)
+    it.setDisplayHomeAsUpEnabled(true)
+  }
+}
